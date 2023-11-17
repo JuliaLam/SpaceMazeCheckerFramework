b3919944fd72852864ab8263a09679f18f7251df
Martin Kellogg <martin.kellogg@njit.edu>
2023-11-17 14:58:10 -0500

    changes from discussion during meeting on 11/17

--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -16,13 +16,15 @@ import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class TileManager {
-    MainPanel gp;
     public Tile[] tile;
     public int[][] mapTileNum;
 
-    public TileManager(@Initialized MainPanel gp){
-        this.gp = gp;
-
+    private final int baseObjectSize, maxColumns, maxRows;
+    
+    public TileManager(final int baseObjectSize, final int maxColumns, final int maxRows){
+        this.baseObjectSize = baseObjectSize;
+        this.maxRows = maxRows;
+        this.maxColumns = maxColumns;
         getTileImage();
         loadMap("/maps/map.txt");
 
@@ -46,8 +48,7 @@ public class TileManager {
     }
     @EnsuresNonNull("this.mapTileNum")
     public void loadMap(@UnderInitialization TileManager this, String filePath) {
-        if (this.gp != null) {
-            mapTileNum = new int[gp.maxColumns][gp.maxRows];
+            mapTileNum = new int[maxColumns][maxRows];
             try {
                 InputStream is = Objects.requireNonNull(getClass().getResourceAsStream(filePath));
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
@@ -55,14 +56,14 @@ public class TileManager {
                 int col = 0;
                 int row = 0;
 
-                while (col < gp.maxColumns && row < gp.maxRows) {
+                while (col < maxColumns && row < maxRows) {
 
                     String line = br.readLine();
                     @SuppressWarnings("nullness") //br is nonempty so .readLine() will never return null
                     @NonNull String nonNullLine = line;
 
 
-                    while (col < gp.maxColumns) {
+                    while (col < maxColumns) {
                         String[] numbers = nonNullLine.split(" ");
 
                         int num = Integer.parseInt(numbers[col]);
@@ -71,7 +72,7 @@ public class TileManager {
                         col++;
                     }
 
-                    if (col == gp.maxColumns) {
+                    if (col == maxColumns) {
                         col = 0;
                         row++;
                     }
@@ -81,8 +82,6 @@ public class TileManager {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-        }
     }
     public void draw(Graphics2D g2){
         int col = 0;
@@ -90,17 +89,17 @@ public class TileManager {
         int x = 0;
         int y = 0;
 
-        while(col < gp.maxColumns && row < gp.maxRows){
+        while(col < maxColumns && row < maxRows){
             int tileNum = mapTileNum[col][row];
-            g2.drawImage(tile[tileNum].image, x, y, gp.baseObjectSize, gp.baseObjectSize, null);
+            g2.drawImage(tile[tileNum].image, x, y, baseObjectSize, baseObjectSize, null);
             col++;
-            x+= gp.baseObjectSize;
+            x+= baseObjectSize;
 
-            if(col == gp.maxColumns){
+            if(col == maxColumns){
                 col = 0;
                 x = 0;
                 row++;
-                y += gp.baseObjectSize;
+                y += baseObjectSize;
 
             }
         }
